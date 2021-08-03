d435bff6015a92a2c08d854fa3b9517a25cd586a
Rafaell Estevam <rafa-estevam@hotmail.com>
2021-08-03 14:27:47 -0300

    alterando interface ProntoSocorroRepository

--- src/main/java/br/com/convenioMedico/sistemaConvenioMedico/repository/ProntoSocorroRepository.java
+++ src/main/java/br/com/convenioMedico/sistemaConvenioMedico/repository/ProntoSocorroRepository.java
@@ -1,5 +1,7 @@
 package br.com.convenioMedico.sistemaConvenioMedico.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +14,6 @@ import br.com.convenioMedico.sistemaConvenioMedico.model.ProntoSocorro;
 @Repository
 public interface ProntoSocorroRepository extends CrudRepository<ProntoSocorro, Long>{
 
-	
+	List<ProntoSocorro>findByCnpj(String cnpj);
 	
 }
